dbd4d8291d15c39996f6b22c08aaa00d2c0bf0e6
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-07-27 15:26:09 -0500

    about update

---
+++ src/components/About/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Leah Bredemeyer</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/99215212?v=4" alt="Leah" />
+
+                <p>
+                I'm Leah, a photographer, graphic designer, Director of Online Product and Website Management, and future coder. I've spent the past 19 years honing my craft and am still learning something new each day. Outside the office, you'll find me with my husband biking on trails, camping in the wilderness and converting a bus into a tiny home. 
+                </p>
+                <p>
+                I graduated with a BA in Journalism from Angelo State University in 2006 with a minor in Graphic Design. I have spent over 15 years working in marketing departments as a Graphic Designer, product photographer, and most recently as a Director of Online Product and Website Management. 
+                </p>
+                <p>
+                Currently, I'm learning coding through University of Arizona bootcamp.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
