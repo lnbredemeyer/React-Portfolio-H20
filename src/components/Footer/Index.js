a029628a25bbd0e8a931b78f5ef2b172e542f934
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-07-27 15:07:39 -0500

    footer update

---
+++ src/components/Footer/Index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer bg-dark fixed-bottom">
+        <a href="https://www.instagram.com/lnbredemeyer/"><img src="https://img.icons8.com/fluency/344/instagram-new.png" alt="personal insta" className="icon"/></a>
+        <a href="https://github.com/lnbredemeyer"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/leah-bredemeyer-b16b6974/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href="https://www.instagram.com/adventuresoflnr/"><img src="https://img.icons8.com/fluency/344/instagram-new.png" alt="travel insta" className="icon"/></a>
+    </footer>
+    )
+}
+
+export default Footer;
