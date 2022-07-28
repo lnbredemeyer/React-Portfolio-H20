cb745734ad935660f0f44b6bb1053dd388d46abf
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-07-28 15:31:16 -0500

    oh more updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
 import Header from './components/Header';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main id="main">
-        <About />
-        <Portfolio />
-        <Skills />
-        <Contact />
+    <div className='portfolio-app'>
+        <Header />
         <Footer />
-      </main>
-    </div >
+    </div>
   );
 }
 
