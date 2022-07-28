eb856516a84d6d4773a5fe2adfad7085fe24b4d5
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-07-28 15:25:14 -0500

    oh more updates again and again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className='portfolio-app'>
-        <Header />
+    <div className="App">
+      <Header />
+      <Hero />
+      <main id="main">
+        <About />
+        <Portfolio />
+        <Skills />
+        <Contact />
         <Footer />
-    </div>
+      </main>
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
