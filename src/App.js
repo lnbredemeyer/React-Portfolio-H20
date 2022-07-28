8a56eec9d90653a8d9efd3256516c572c02ed9bf
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-07-28 14:55:54 -0500

    lots of code update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Redirect} from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Navigation from "../../components/Navigation";
 import About from "../../components/About";
 import Portfolio from "../../components/Portfolio";
